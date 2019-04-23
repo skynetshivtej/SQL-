0a00e6e5aacd74cb052d845445302830849c9806
ShivtejPatil <37193739+skynetshivtej@users.noreply.github.com>
2019-04-23 16:58:24 +0100

    print the numbers

---
+++ Phone Number Cleaning.sql
@@ -0,0 +1,17 @@
+﻿BEGIN
+  SET NOCOUNT ON;
+  DECLARE @query nvarchar(max) =  
+  N'SELECT top(100) RemoteID from InteractionSummary'  
+  EXECUTE sp_execute_external_script @language = N'R',  
+                                     @script = N'  
+
+   #Plot histogram  
+   a<- InputDataSet
+   
+    
+   OutputDataSet <- data.frame(a);  
+   ',  
+   @input_data_1 = @query  
+   WITH RESULT SETS ((plot varchar(max)));  
+END
+GO
