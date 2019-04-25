76d99430ba65720ba7ece7e0aded98dd38190393
ShivtejPatil <37193739+skynetshivtej@users.noreply.github.com>
2019-04-25 12:44:49 +0100

    Joining Two tables

---
+++ Join Two tables.sql
@@ -0,0 +1,10 @@
+﻿/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT a.[InteractionIDKey]
+     ,[UserID],
+     b.[WorkgroupID]
+      ,b.[WrapupCode]
+      ,b.[WrapupCategory],
+	  b.[tConnected]
+      ,[WrapupStartDateTimeUTC],
+	   a.[Direction]
+      FROM [Core].[dbo].[InteractionSummary] a JOIN [Core].[dbo].[InteractionWrapup] b ON a.InteractionIDKey = b.InteractionIDKey;
