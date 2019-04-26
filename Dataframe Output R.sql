0b5db5a89ed2b3a842118cb37d1b074b340b62a4
ShivtejPatil <37193739+skynetshivtej@users.noreply.github.com>
2019-04-26 17:29:38 +0100

    R in SQL Machine Learning Services

--- Dataframe Output R.sql
+++ Dataframe Output R.sql
@@ -3,14 +3,18 @@
   DECLARE @query nvarchar(max) =  
   N'SELECT a.[InteractionIDKey]
      ,[UserID]
-   
+     
+      ,cast([WrapupStartDateTimeUTC] as datetime) 
+	 
+	   
       FROM [Core].[dbo].[InteractionSummary] a JOIN [Core].[dbo].[InteractionWrapup] b ON a.InteractionIDKey = b.InteractionIDKey; '  
   EXECUTE sp_execute_external_script @language = N'R',  
                                      @script = N'  
 									 
 									 data =InputDataSet
-									 OutputDataSet = data.frame(data[,1:2])
+									 print(class(data))
+									 OutputDataSet = data.frame(data[,1:3])
 									 ',
 									 @input_data_1 = @query
-									 with result SETS ((interaction varchar(40), id varchar(50)))
+									 with result SETS ((interaction varchar(40), id varchar(50),dob datetime ))
 END
