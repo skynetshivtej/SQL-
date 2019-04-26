badde5d0aa01374fe70ec6aceb58b341af4c08e6
ShivtejPatil <37193739+skynetshivtej@users.noreply.github.com>
2019-04-26 15:47:29 +0100

    R in SQL Machine Learning Services

---
+++ Dataframe Output R.sql
@@ -0,0 +1,16 @@
+﻿BEGIN
+  SET NOCOUNT ON;
+  DECLARE @query nvarchar(max) =  
+  N'SELECT a.[InteractionIDKey]
+     ,[UserID]
+   
+      FROM [Core].[dbo].[InteractionSummary] a JOIN [Core].[dbo].[InteractionWrapup] b ON a.InteractionIDKey = b.InteractionIDKey; '  
+  EXECUTE sp_execute_external_script @language = N'R',  
+                                     @script = N'  
+									 
+									 data =InputDataSet
+									 OutputDataSet = data.frame(data[,1:2])
+									 ',
+									 @input_data_1 = @query
+									 with result SETS ((interaction varchar(40), id varchar(50)))
+END
