16ba519a0109f64957bb9558f8597667fef244e9
ShivtejPatil <37193739+skynetshivtej@users.noreply.github.com>
2019-05-10 10:12:52 +0100

    Create Create view .sql
    create view

---
+++ Create view .sql
@@ -0,0 +1,37 @@
+create view callhistv
+As
+SELECT [campaignname] 
+
+      ,[phonenumber] 
+
+      ,cast([callplacedtimeUTC] as date) AS calldate 
+
+      ,cast([callansweredtimeUTC] as smalldatetime) AS callanswered 
+
+      ,[messageplaytimeUTC] 
+
+      ,cast([callconnectedtimeUTC] as smalldatetime) AS callconnected 
+
+      ,[agentid] 
+
+      ,cast([calldisconnectedtimeUTC] as smalldatetime) AS callended 
+
+      ,[length] 
+
+      ,[isabandoned] 
+
+      ,[iscontact] 
+
+      ,[wrapupcategory] 
+
+      ,[wrapupcode] 
+
+	  ,CONTACT,
+	  DMC,
+	  SALE
+
+  FROM [Core].[ININ_DIALER_40].[CallHistory] ch 
+
+INNER JOIN   
+
+[Important].[dbo].[outcome] ot on ch. Wrapupcode = ot. LocalizedWrapUpCode ;
